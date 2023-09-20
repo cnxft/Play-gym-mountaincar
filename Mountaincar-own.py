8b7071b86ff781f5c55d48885b56fbdeeb990aca
从小就瓜 <101243557+cnxft@users.noreply.github.com>
2023-09-20 20:51:42 +0800

    Update Mountaincar-own.py

--- Mountaincar-own.py
+++ Mountaincar-own.py
@@ -3,7 +3,8 @@ import pygame
 from gym.utils.play import play
 
 # 定义案件的映射
-mapping = {(pygame.K_LEFT,): 0, (pygame.K_RIGHT,): 1}
+# 0往左，1不动（我没放），2往右
+mapping = {(pygame.K_LEFT,): 0, (pygame.K_RIGHT,): 2}
 
 # 直接玩这个游戏
 env = gym.make('MountainCar-v0', render_mode='rgb_array')
